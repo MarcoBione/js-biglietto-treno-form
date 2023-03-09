cbd3cc1f16115b74870de2ccbd82a10b8b7d185e
MarcoBione <marcobione.work@gmail.com>
2023-03-09 17:13:34 +0100

    Add some variables in JS

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,35 @@
+//---Variables---
+
 //Username&Surname
 let nameSurname = document.getElementById(nameSurname);
 //check name&surname
 console.log(nameSurname);
+
+//KmDistance
+let KmDistance = document.getElementById(Kmdistance);
+//check KmDistance
+console.log(KmDistance);
+
+//KmPrice
+const KmPrice = 0.21;
+//check KmPrice
+console.log(KmPrice);
+
+//DiscountJunior
+const DJunior = 0.20;
+//check DJunior
+console.log(DJunior);
+
+//DiscountSenior
+const DSenior = 0.40;
+//check DSenior
+console.log(DSenior);
+
+
+//---End variables---
+
+
+//---Main---
+
+
+//---End Main---
